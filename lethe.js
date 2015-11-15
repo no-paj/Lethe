80ef3f1058eebf8a10e66b9896d1f8445bb349ee
no-paj <nopaj@openmailbox.org>
2015-11-16 00:44:07 +0100

    Added simple http server

--- lethe.js
+++ lethe.js
@@ -24,6 +24,19 @@ var botMention = false;
 client.on('ready', () => {
   botMention = `<@${client.internal.user.id}>`;
   console.log(`Bot mention: ${botMention}`);
+	  //step 1) specify that we need the ‘http’ module
+	var http = require('http');
+
+	  //step 2) start the server
+	  http.createServer(function (req, res) {
+
+	  //set an HTTP header of 200 and the meta type
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+	  //write something to the request and end it
+	  res.end('Your node.js server is running on localhost:3000');
+
+	}).listen(3000);//step 3) listen for a request on port 3000
 });
 
 client.on('message', m => {
